3c4426c0dd225cfc7e40d5ec4b48b2a4d7718fa9
rodrigomin <rodrigo.minotic@gmail.com>
2026-03-27 11:16:04 -0300

    FIX: Trocando o webDir de 'dist' para 'build'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.economix.app',
   appName: 'EconoMix',
-  webDir: 'dist'
+  webDir: 'build'
 };
 
 export default config;
